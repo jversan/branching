76edc8f7d98f2536aedabf8faf62d23a178d1f6d
jversan <jversan516@iesalmudeyne.es>
2023-11-10 10:50:30 +0100

    holaMundo.java

---
+++ HolaMundo.java
@@ -0,0 +1,9 @@
+/**
+ * Muestra por pantalla la frase "Hola Mundo"
+ */
+public class HolaMundo {
+    public static void main (Strinf[] args) {
+        System.out.println("¡Hola Mundo!");
+        System.out.println("¡Aquí estamos haciendo pruebas en Gitg");
+    }
+}
