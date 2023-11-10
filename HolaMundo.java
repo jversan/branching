ecb3c92dee9963c592f6a5ebfad36d13a1bb4869
jversan <jversan516@iesalmudeyne.es>
2023-11-10 10:59:55 +0100

    colores

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,6 +3,8 @@
  */
 public class HolaMundo {
     public static void main (Strinf[] args) {
+        String rojo = "\033[32m";
+        String azul = "\033[34m";
         System.out.println("¡Hola Mundo!");
         System.out.println("¡Aquí estamos haciendo pruebas en Gitg");
     }
