56caea9f659f2b2608cde962c645c010143cf521
jversan <jversan516@iesalmudeyne.es>
2023-11-17 10:39:11 +0100

    Se ha cambiado el código \033[32m por el código correcto \033[31m

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,9 +3,9 @@
  */
 public class HolaMundo {
     public static void main (String[] args) {
-        String rojo = "\033[32m";
+        String rojo = "\033[31m";
         String azul = "\033[34m";
         System.out.println(rojo +"¡Hola Mundo!");
-        System.out.println(azul +"¡Aquí estamos haciendo pruebas en Git");
+        System.out.println(azul +"Aquí estamos haciendo pruebas en Git.");
     }
 }
